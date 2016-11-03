8c680de1d2d753cacab8b7b98e596eb404879623
MONTSERRAT MORALES <MONTSERRAT MORALES>
2016-11-03 08:11:23 -0600

    clase del martes 1/11/16

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var colors = require('colors');
 var fs = require('fs');
+var mime = require('mime');
 
 //cargando configuraciones
 var config = require('./config/config.js');
@@ -11,12 +12,40 @@ var PORT = config.PORT;
 //colors.setTheme(config.color_theme);
 
 var server = http.createServer(function(req, res){// codigo de nuestro server
+//extrayendo el path de la URL
+var path = req.url;
+//normalizando el path
+if(path==="/"){
+    path ="./static/index.html";
+}else{
+    path ='./static'+path;
+}
+console.log(`>Recurso Solicitado: ${path}`);
+//Decidiendo el content.Type en funcion de la extencion del archivo solicitado
+var ext = path;
+var resp = ext.split(".");
 
+
+switch(ext[2]){
+    case'html':
     res.writeHead(200,{
         'Content-Type':'text/html'
     });
-
-fs.readFile('./static/index.html','utf8', 
+    breack;
+    case'js':
+    res.writeHead(200,{
+        'Content-Type':'text/javascript'
+    });
+breack;
+case'css':
+res.writeHead(200,{
+    'Content-Type':'text/css'
+});
+breack;
+default:
+break;
+}
+fs.readFile(path,'utf8', 
 function(err, Content){
     if(err){
     console.log(`> Error al leer archivo: ${err}`);
@@ -26,6 +55,8 @@ function(err, Content){
     res.end("Error 500: Iternal Error...");
 }else{
     //TODO: si sirve el archivo
+    console.log(">Se sirve el archivo: ./static/index.html");
+res.end(Content);
 }
 
 });
